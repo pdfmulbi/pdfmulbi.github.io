212cbc86e5e2b2d310bf03a4d1ef2bc86dbbc81b
gitautami <utamigita797@gmail.com>
2025-01-05 19:35:14 +0700

    edit menu dan responsif

--- js/profile.js
+++ js/profile.js
@@ -8,14 +8,3 @@ document.getElementById('cancel-edit').addEventListener('click', function () {
     document.getElementById('profile-edit').style.display = 'none';
     document.getElementById('profile-view').style.display = 'block';
 });
-
-document.getElementById('edit-password').addEventListener('click', function (e) {
-    e.preventDefault();
-    document.getElementById('profile-view').style.display = 'none';
-    document.getElementById('password-edit').style.display = 'block';
-});
-
-document.getElementById('cancel-password-edit').addEventListener('click', function () {
-    document.getElementById('password-edit').style.display = 'none';
-    document.getElementById('profile-view').style.display = 'block';
-});
