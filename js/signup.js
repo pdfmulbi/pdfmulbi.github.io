99b1954f198d83c8aa0dc71a436156d196075635
Sam9100 <mhaitsamia@gmail.com>
2025-01-06 22:09:42 +0700

    add online register func

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,49 @@
+import { postJSON } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/api.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const registerButton = document.getElementById("registerButton");
+
+    registerButton.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const confirmPassword = document.getElementById("confirmPassword").value;
+
+        // Basic validation
+        if (!name || !email || !password || !confirmPassword) {
+            alert("Silakan isi semua kolom.");
+            return;
+        }
+
+        if (password !== confirmPassword) {
+            alert("Kata sandi dan konfirmasi kata sandi tidak cocok.");
+            return;
+        }
+
+        // Prepare data
+        const data = {
+            name: name,
+            email: email,
+            password: password,
+        };
+
+        // Send data to server
+        const target_url = 
+        "https://asia-southeast1-pdfmulbi.cloudfunctions.net/pdfmulbi/pdfm/register";
+
+        postJSON(target_url, "Content-Type", "application/json", data, function (response) {
+            if (response.status >= 200 && response.status < 300) {
+                alert("Pendaftaran berhasil!");
+                document.querySelector("form").reset();
+            } else {
+                alert("Terjadi kesalahan: " + response.data.message);
+            }
+        });
+    });
+});
+
 document.addEventListener('DOMContentLoaded', function () {
     const passwordInput = document.getElementById('password');
     const togglePassword = document.getElementById('togglePassword');
@@ -22,4 +68,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
-
